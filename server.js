6e04ca1ca665573b3d10160f93011fbe47bfc038
Ayomal98 <ayomalpraveen33@gmail.com>
2021-07-15 17:45:29 +0530

    connection created for atlas

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = parseInt(process.env.PORT) || 3100;
+const URI = process.env.MONGO_URI;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", (req, res) => {
+  console.log("Its running");
+});
+
+const connectDB = async () => {
+  console.log(URI);
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to database");
+  } catch (error) {
+    console.log("Error");
+  }
+};
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
