f92285b16e8f08c4838c2ff7ece8ee69adbd60a7
Ayomal98 <ayomalpraveen33@gmail.com>
2021-07-15 20:45:57 +0530

    update product

---
+++ controllers/updateProduct.controller.js
@@ -0,0 +1,12 @@
+const Product = require("../models/product.model");
+
+module.exports = async (req, res) => {
+  const { id } = req.params;
+  const { name, quantity, type, price } = req.body;
+  try {
+    await Product.findByIdAndUpdate(id, { name, quantity, type, price });
+    res.status(200).json({ message: "Product updated successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
--- routes/products.js
+++ routes/products.js
@@ -5,12 +5,14 @@ const addProduct = require("../controllers/addProduct.controller");
 const getAllProducts = require("../controllers/getProducts.controller");
 const getProduct = require("../controllers/getProduct.controller");
 const deleteProduct = require("../controllers/deleteProduct.controller");
-const deleteProducts = require("../controllers/deleteProducts.controller");
+const deleteProducts = require("../controllers/deleteAllProducts.controller");
+const updateProduct = require("../controllers/updateProduct.controller");
 
 router.get("/", getAllProducts);
 router.get(":id", getProduct);
 router.post("/addproduct", addProduct);
 router.delete("/deleteproduct/:id", deleteProduct);
 router.delete("/deleteProducts", deleteProducts);
+router.put("/updateProduct/:id", updateProduct);
 
 module.exports = router;
